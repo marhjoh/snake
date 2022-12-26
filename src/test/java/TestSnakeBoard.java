68198d82fac50ff10c821c767684c2ae2b3e1702
Martin Hegnum Johannessen <marhjoh@stud.ntnu.no>
2022-12-26 17:50:23 +0100

    modified the tests in TestSnakeBoard.

--- src/test/java/TestSnakeBoard.java
+++ src/test/java/TestSnakeBoard.java
@@ -8,20 +8,22 @@ class TestSnakeBoard {
 
     // A dummy snakeBoard for testing the methods
     private SnakeBoard snakeBoard;
-    private SnakeJoint snakeJoint;
 
     @BeforeEach
     @DisplayName("This method will create objects for the tests before each test")
     void setup() {
         snakeBoard = new SnakeBoard();
-        snakeJoint = new SnakeJoint((int) (Math.random() * 400), (int) (Math.random() * 400));
     }
 
     @Test
-    @DisplayName("This method will test the SnakeBoard constructor")
-    void testSnakeBoardConstructor(){
-        assertEquals(3, snakeBoard.getSnake().size());
+    @DisplayName("This method will test the initial length of the snake")
+    void testInitialSnakeLength() {
+        assertEquals(SnakeBoard.INITIAL_SNAKE_LENGTH, snakeBoard.getSnake().size());
+    }
+
+    @Test
+    @DisplayName("This method will test the initial direction of the snake")
+    void testInitialDirection() {
         assertEquals(SnakeDirection.RIGHT, snakeBoard.getDirection());
-        assertNotEquals(snakeJoint.toString(), snakeBoard.getApple().toString());
     }
 }
