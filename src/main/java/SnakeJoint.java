da38c6d6a86a4939feab1c9a34242da889cda3ed
Martin Hegnum Johannessen <marhjoh@stud.ntnu.no>
2022-12-26 01:40:54 +0100

    modified the equals() and hashCode() methods in SnakeJoint

--- src/main/java/SnakeJoint.java
+++ src/main/java/SnakeJoint.java
@@ -32,4 +32,36 @@ public class SnakeJoint {
         return y;
     }
 
+    /**
+     * Compares this SnakeJoint object to the specified object.
+     * The result is true if and only if the argument is not null
+     * and is a SnakeJoint object with the same x and y coordinates as this object.
+     *
+     * @param o the object to compare this SnakeJoint against
+     * @return true if the given object represents a SnakeJoint equivalent to this object, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof SnakeJoint)) {
+            return false;
+        }
+        SnakeJoint other = (SnakeJoint) o;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    /**
+     * Returns a hash code for this SnakeJoint object.
+     *
+     * @return a hash code value for this object
+     */
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + x;
+        result = 31 * result + y;
+        return result;
+    }
 }
